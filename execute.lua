58cf7a1efee56c647aac4ef7ba4529c9a867b252
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-12-30 00:31:42 +0200

    remove unnecessary comments

--- execute.lua
+++ execute.lua
@@ -1,8 +1,6 @@
 function CodeBlock(block)
-  -- Check if the code block is Python, C++, JavaScript, or Java
   local lang = block.classes[1]
   if lang == "python" or lang == "cpp" or lang == "javascript" or lang == "java" then
-    -- If the code block has no identifier, skip it
     if not block.identifier or block.identifier == "" then
       return block
     end
@@ -18,27 +16,21 @@ function CodeBlock(block)
       output_file = block.identifier .. "_output.txt"
       executable = "python"
     
-      -- Save the code to a source file
       local source_handle = io.open(source_file, "w")
       source_handle:write(block.text)
       source_handle:close()
     
-      -- Run the code block using pandoc.pipe and capture the output
       local handle = io.popen(executable .. " " .. source_file .. " 2>&1") -- Capture both stdout and stderr
       local output_text = handle:read("*a")
       handle:close()
     
-      -- Create a code block with the output using pandoc.CodeBlock
       local output_block = pandoc.CodeBlock(output_text, "text")
     
-      -- Append the output block after the code block using pandoc.List
       local blocks = pandoc.List({block, output_block})
     
-      -- Cleanup: remove temporary files
       os.remove(source_file)
       os.remove(output_file)
     
-      -- Return the blocks
       return blocks
     
     elseif lang == "cpp" then
@@ -47,12 +39,10 @@ function CodeBlock(block)
       output_file = block.identifier .. "_output.txt"
       executable = "./" .. block.identifier
 
-      -- Save the code to a source file
       local source_handle = io.open(source_file, "w")
       source_handle:write(block.text)
       source_handle:close()
 
-      -- Compile the C++ code
       os.execute("g++ -o " .. block.identifier .. " " .. source_file)
     elseif lang == "javascript" then
       file_extension = "js"
@@ -60,7 +50,6 @@ function CodeBlock(block)
       output_file = block.identifier .. "_output.txt"
       executable = "node"
 
-      -- Save the code to a source file
       local source_handle = io.open(source_file, "w")
       source_handle:write(block.text)
       source_handle:close()
@@ -70,34 +59,27 @@ function CodeBlock(block)
       output_file = block.identifier .. "_output.txt"
       executable = "java"
 
-      -- Save the code to a source file
       local source_handle = io.open(source_file, "w")
       source_handle:write(block.text)
       source_handle:close()
     end
 
-    -- Run the code block using pandoc.pipe and capture the output
     local handle = io.popen(executable .. " " .. source_file .. " 2>&1") -- Capture both stdout and stderr
     local output_text = handle:read("*a")
     handle:close()
 
-    -- Create a code block with the output using pandoc.CodeBlock
     local output_block = pandoc.CodeBlock(output_text, "text")
 
-    -- Append the output block after the code block using pandoc.List
     local blocks = pandoc.List({block, output_block})
 
-    -- Cleanup: remove temporary files
     os.remove(source_file)
     os.remove(output_file)
     if lang == "cpp" or lang == "java" then
       os.remove(block.identifier)
     end
 
-    -- Return the blocks
     return blocks
   end
 
-  -- Otherwise, return the original block
   return block
 end
